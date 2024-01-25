e31f71a8b935338311ee8ee08530eb4d73a9df4f
SebastianSroczyk <sroczyk.sebastian@gmail.com>
2024-01-25 10:02:04 +0000

    Rebuilding Template

--- Template Project/StudentGame/obj/Debug/net6.0/StudentGame.AssemblyInfo.cs
+++ Template Project/StudentGame/obj/Debug/net6.0/StudentGame.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StudentGame")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aa9188b687b6383f9986fa1e68afc180b6bb3c76")]
 [assembly: System.Reflection.AssemblyProductAttribute("StudentGame")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StudentGame")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
